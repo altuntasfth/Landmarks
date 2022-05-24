224adb3d0001442b7004ee2129ee397d1fc0cf87
Fatih <fatih.altuntas@noho.games>
2022-05-24 23:47:36 +0300

    to allow the map content to extend to the top edge of the screen, added the ignoresSafeArea(edges: .top) modifier to the map view.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             MapView()
+                .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
             
             CircleImage()
