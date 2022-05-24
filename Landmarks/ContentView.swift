ffc8950031ce873c867b38541aed61d95aa5c6ac
Fatih <fatih.altuntas@noho.games>
2022-05-24 23:37:03 +0300

    adjusted the circle image offset and padding value.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -12,10 +12,15 @@ struct ContentView: View {
         VStack {
             MapView()
                 .frame(height: 300)
+            
             CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
+                
                 HStack {
                     Text("Joshua Tree National Park")
                         .font(.subheadline)
