cd360b6f346cb7b29e6830b92306fac724176dc2
Fatih <fatih.altuntas@noho.games>
2022-06-10 16:24:02 +0300

    moved the landmarks array into the model.

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -9,10 +9,8 @@ import Foundation
 import Combine
 
 final class ModelData: ObservableObject {
-    
+    var landmarks: [Landmark] = load("landmarkData.json")
 }
-
-var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
